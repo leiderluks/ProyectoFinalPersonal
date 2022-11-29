85402509ef5ac5c179c82b4926edd6afe1aed274
407 <407@academicos.cesde.edu.co>
2022-11-29 16:39:08 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,73 @@
+import "./Home.css"
+
+export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class= "banner">
+
+                </div>
+            </section>
+            <section>
+                <div class= "container">
+                    <div class="row">
+                        <div class="col-12 col-md-8 container">
+                            <h2>AC/DC</h2>
+                            <p>AC/DC es una banda de hard rock británica-australiana, formada en 1973 en Australia por los hermanos escoceses Malcolm Young y Angus Young. Sus álbumes se han vendido en un total estimado de 200 millones de copias, embarcándose en giras multitudinarias por todo el mundo y sus éxitos han musicalizado varias producciones cinematográficas sobresalientes. Son famosas sus actuaciones en vivo, resultando vibrantes y exultantes espectáculos de primer orden. Mucho de ello se debe al extravagante estilo de su guitarrista principal y símbolo visual, Angus Young, quien asume el rol de guitarrista principal durante los conciertos, gracias a sus dinámicos y adrenalínicos despliegues escénicos uniformado de colegial callejero.</p>
+                            <div >
+                                <img class="concierto"src="https://firebasestorage.googleapis.com/v0/b/spotyllp.appspot.com/o/concert.jpg?alt=media&token=b64034ac-499a-4b21-a0aa-b487af59cf45" alt="concierto"/>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 d-grid gap-3 align-baseline">
+                        <i class="bi bi-music-note-list ">
+                            <h5>Medellin</h5>
+                            <h5>Dic: 3</h5>
+                        </i>
+
+                        <i class="bi bi-music-note-list">
+                            <h5>Medellin</h5>
+                            <h5>Dic: 3</h5>
+                        </i>
+
+                        <i class="bi bi-music-note-list">
+                            <h5>Medellin</h5>
+                            <h5>Dic: 3</h5>
+                        </i>
+
+                        <i class="bi bi-music-note-list">
+                            <h5>Medellin</h5>
+                            <h5>Dic: 3</h5>
+                        </i>
+
+                        </div>
+
+                    </div>
+                    
+                </div>
+                
+            </section>
+
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyllp.appspot.com/o/noticia1.webp?alt=media&token=43bca0e2-45fc-4559-b06c-ca01860679ef" alt="" class="img-fluid w-100"></img>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyllp.appspot.com/o/noticia4.jpg?alt=media&token=629c51fc-1335-4145-b6d1-862af6b3006a" alt="" class="img-fluid w-100"></img>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyllp.appspot.com/o/noticia3.jpg?alt=media&token=25f81b2b-1974-4347-9356-9eafe01df14e" alt="" class="img-fluid w-100"></img>
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+
+            </section>
+
+        </>
+    )
+}
